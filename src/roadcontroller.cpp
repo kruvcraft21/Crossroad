900229781fee25528f99b89ef4d1c8a128e85bfe
Kruvcraft <kruvcraft@yandex.ru>
2023-06-19 10:14:33 +0500

    Fixed bug deleting machines

--- src/roadcontroller.cpp
+++ src/roadcontroller.cpp
@@ -12,7 +12,7 @@ Road_Controller::Road_Controller() {
     Vector2 left_dir = {1, 0};
     Road left(left_rect, left_pos, left_dir, left_mark);
     // Добавляем дорогу в коллекцию под именем "left"
-    road_collection.insert(make_pair("left", left));
+    road_collection.insert({"left", left});
 
     // Создаем дорогу сверху от центра
     Rectangle up_rect = {200, 0, 100, 200};
@@ -21,7 +21,7 @@ Road_Controller::Road_Controller() {
     Mark up_mark = {up_b, up_e};
     Vector2 up_dir = {0, 1};
     Road up(up_rect, up_pos, up_dir, up_mark);
-    road_collection.insert(make_pair("up", up));
+    road_collection.insert({"up", up});
 
     // Создаем дорогу снизу от центра
     Rectangle botton_rect = {200, 300, 100, 200};
@@ -30,7 +30,7 @@ Road_Controller::Road_Controller() {
     Mark botton_mark = {botton_b, botton_e};
     Vector2 botton_dir = {0, -1};
     Road botton(botton_rect, botton_pos, botton_dir, botton_mark);
-    road_collection.insert(make_pair("button", botton));
+    road_collection.insert({"button", botton});
 
     // Создаем дорогу справа от центра
     Rectangle right_rect = {300, 200, 200, 100};
@@ -39,13 +39,13 @@ Road_Controller::Road_Controller() {
     Mark right_mark = {right_b, right_e};
     Vector2 right_dir = {-1, 0};
     Road right(right_rect, right_pos, right_dir, right_mark);
-    road_collection.insert(make_pair("right", right));
+    road_collection.insert({"right", right});
 
     // Создаем центральную дорогу
     Rectangle center_rect = {200, 200, 100, 100};
     Vector2 center_pos = {0, 0};
     Road center(center_rect, center_pos);
-    road_collection.insert(make_pair("center", center));
+    road_collection.insert({"center", center});
 
     // Создаем знаки дорожного движения
     traffic_signs.push_back(Traffic_sign("resources/glavnaya.png", {150, 150}));
@@ -62,40 +62,38 @@ void Road_Controller::Start() {
             ClearBackground(WHITE);
 
             // Отрисовываем все дороги
-            for (auto road = road_collection.begin(); road != road_collection.end(); road++)
+            for (auto &road : road_collection)
             {
-                road->second.Draw();
+                road.second.Draw();
             } 
 
             // Отрисовываем все знаки дорожного движения
-            for (auto traffic_sign = traffic_signs.begin(); traffic_sign != traffic_signs.end(); traffic_sign++)
+            for (auto &traffic_sign : traffic_signs)
             {
-                traffic_sign->Draw();
+                traffic_sign.Draw();
             }
 
             // Добавляем новые машины на дорогу
             AddCars();
 
             // Движение и отрисовка всех простых машин на дороге
-            for (int i = 0; i < simple_cars.size(); i++) {
-                simple_cars[i].Draw();
-                simple_cars[i].Drive();
-                if (CheckCar(simple_cars[i].get_pos())) 
-                {
-                    simple_cars.erase(simple_cars.begin() + i);
-                }
+            for (auto car = simple_cars.begin(); car != simple_cars.cend();)
+            {
+                car->Draw();
+                car->Drive();
+                // Удаляем машину если она вышла за границы экрана
+                car = CheckCar(car->get_pos()) ? simple_cars.erase(car) : car + 1;
             }
 
             // Движение и отрисовка всех специальных машин на дороге
-            for (int i = 0; i < spec_cars.size(); i++)
+            for (auto car = spec_cars.begin(); car != spec_cars.end();
+                 // Удаляем машину если она вышла за границы экрана
+                 car = CheckCar(car->get_pos()) ? spec_cars.erase(car) : car + 1)
             {
-                spec_cars[i].Draw();
-                spec_cars[i].Drive();
-                if (CheckCar(spec_cars[i].get_pos()))
-                {
-                    spec_cars.erase(spec_cars.begin() + i);
-                }
+                car->Draw();
+                car->Drive();
             }
+
         EndDrawing();
     }
 }
